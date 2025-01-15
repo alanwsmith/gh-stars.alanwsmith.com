4c56facf30d8f057152fd06e17dff53f15c94f99
Alan Smith <commit@alanwsmith.com>
2025-01-14 22:44:20 -0500

    Starting to build button with gh logo and star icon

--- site/scripts/gh-stars.js
+++ site/scripts/gh-stars.js
@@ -5,21 +5,33 @@ class GitHubStars extends HTMLElement {
   }
 
   async init() {
-    await this.fetchJson(`https://api.github.com/repos/${this.repo}`)
-    this.button = document.createElement("button")
-    this.button.classList.add("gh-stars-button")
-    this.appendChild(this.button)
-    console.log(this.gitHubJson.stargazers_count)
+
+    //await this.fetchJson(`https://api.github.com/repos/${this.repo}`)
+    this.ghButton = document.createElement("button")
+    this.ghButton.classList.add("gh-stars-button")
+    this.appendChild(this.ghButton)
+    //this.button.style = `margin: 0; background: yellow; height: 50px;`
+    //this.wrapper = document.createElement("div")
+    //this.wrapper.style = `background: red;`
+    // //this.wrapper.style = `background: red; border: 1px solid blue; display: inline-block; margin: 0; padding: 0;`
+    //// this.wrapper.style = `background: red; border: 1px solid blue; display: inline-block; margin: 0; padding: 0;`
+   //// this.wrapper.appendChild(this.button)
+    ////console.log(this.gitHubJson.stargazers_count)
+    ////
+
+
   }
 
   async fetchJson(url) {
-    let response = await fetch(url, {headers: {'Accept' : 'application/vnd.github.v3+json'}})
-    if (!response.ok) {
-      throw new Error('There was a problem getting the data')
-    } else {
-      this.gitHubJson = await response.json()
-      // TODO: Figure out error handling here
-    }
+
+    // let response = await fetch(url, {headers: {'Accept' : 'application/vnd.github.v3+json'}})
+    // if (!response.ok) {
+    //   throw new Error('There was a problem getting the data')
+    // } else {
+    //   this.gitHubJson = await response.json()
+    //   // TODO: Figure out error handling here
+    // }
+
   }
 }
 
